8c3ba52844a8bc6a6adbfc25a86a89fdf1777c65
Volker Fischer <corrados@users.noreply.github.com>
2019-06-27 06:57:01 +0200

    my new Pi setup uses GPIO 22 for IR output

--- ledremote.c
+++ ledremote.c
@@ -149,7 +149,7 @@ static inline int irSling(uint32_t outPin,
 
 int main(int argc, char *argv[])
 {
-    uint32_t outPin = 24;            // The Broadcom pin number the signal will be sent on
+    uint32_t outPin = 22;            // The Broadcom pin number the signal will be sent on
     int frequency = 38000;           // The frequency of the IR signal in Hz
     double dutyCycle = 0.5;          // The duty cycle of the IR signal. 0.5 means for every cycle,
                                      // the LED will turn on for half the cycle time, and off the other half
