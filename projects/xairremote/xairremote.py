110c1cf02e75f15058341c8a5e862f7a0f42e403
Volker Fischer <corrados@users.noreply.github.com>
2022-04-30 21:32:55 +0200

    parse MIDI events

--- projects/xairremote/xairremote.py
+++ projects/xairremote/xairremote.py
@@ -4,27 +4,34 @@
 # control a Behringer XAIR mixer with a nanoKONTROL connected to a Raspberry Pi
 
 from re import match
-from alsa_midi import SequencerClient, WRITE_PORT, NoteOnEvent, NoteOffEvent
-import time
+from alsa_midi import SequencerClient, WRITE_PORT, MidiBytesEvent, NoteOnEvent, NoteOffEvent
 
 
 def main():
   # setup the MIDI sequencer client for xairremote
   client = SequencerClient("xairremote")
-  port   = client.create_port("output", caps=WRITE_PORT)
+  port   = client.create_port("output", caps = WRITE_PORT)
+  queue  = client.create_queue()
+  queue.start()
+  client.drain_output()
 
   # get the nanoKONTROL ALSA midi port
-  dest_ports    = client.list_ports(output=True)
+  dest_ports    = client.list_ports(output = True)
   filtered_port = list(filter(lambda cur_port: match('\W*(nanoKONTROL)\W*', cur_port.name), dest_ports))
   if len(filtered_port) == 0:
     raise Exception('No nanoKONTROL MIDI device found. Is the nanoKONTROL connected?')
   nanoKONTROL_port = filtered_port[0];
   port.connect_from(nanoKONTROL_port)
 
-  # TODO from here...
-  print(nanoKONTROL_port.name)
-  time.sleep(3)
-
+  # parse MIDI inevents
+  try:
+    while True:
+      event = client.event_input(prefer_bytes = True)
+      if event is not None and isinstance(event, MidiBytesEvent):
+        event_s = " ".join(f"{b:02X}" for b in event.midi_bytes)
+        print(f"{event.source!s:7} {event_s}")
+  except KeyboardInterrupt:
+    pass
 
 
 if __name__ == '__main__':
