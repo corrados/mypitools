c2f9f0298173d9e31f29af7874468d4d33df561d
Volker Fischer <corrados@users.noreply.github.com>
2022-04-30 21:19:47 +0200

    introduce main function and connect ALSA MIDI ports

--- projects/xairremote/xairremote.py
+++ projects/xairremote/xairremote.py
@@ -5,20 +5,29 @@
 
 from re import match
 from alsa_midi import SequencerClient, WRITE_PORT, NoteOnEvent, NoteOffEvent
+import time
 
-# setup the MIDI sequencer client for xairremote
-client = SequencerClient("xairremote")
-port   = client.create_port("output", caps=WRITE_PORT)
 
-# get the nanoKONTROL ALSA midi port
-dest_ports    = client.list_ports(output=True)
-filtered_port = list(filter(lambda cur_port: match('\W*(nanoKONTROL)\W*', cur_port.name), dest_ports))
-if len(filtered_port) == 0:
-  raise Exception('No nanoKONTROL MIDI device found. Is the nanoKONTROL connected?')
-dest_port = filtered_port[0];
+def main():
+  # setup the MIDI sequencer client for xairremote
+  client = SequencerClient("xairremote")
+  port   = client.create_port("output", caps=WRITE_PORT)
 
-# TODO from here...
-print(filtered_port[0].name)
+  # get the nanoKONTROL ALSA midi port
+  dest_ports    = client.list_ports(output=True)
+  filtered_port = list(filter(lambda cur_port: match('\W*(nanoKONTROL)\W*', cur_port.name), dest_ports))
+  if len(filtered_port) == 0:
+    raise Exception('No nanoKONTROL MIDI device found. Is the nanoKONTROL connected?')
+  nanoKONTROL_port = filtered_port[0];
+  port.connect_from(nanoKONTROL_port)
 
+  # TODO from here...
+  print(nanoKONTROL_port.name)
+  time.sleep(3)
+
+
+
+if __name__ == '__main__':
+    main()
 
 
