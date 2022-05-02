cbeea91d88223751caa9e3d96f6adb9305db8cad
Volker Fischer <corrados@users.noreply.github.com>
2022-05-02 06:58:00 +0200

    created nanoKONTROL lookup table

--- projects/xairremote/xairremote.py
+++ projects/xairremote/xairremote.py
@@ -27,11 +27,9 @@ def main():
   nanoKONTROL_port = filtered_port[0];
   port.connect_from(nanoKONTROL_port)
 
-  # TEST
-  mixer = x32.BehringerX32("127.0.0.1", 10330, False)
-  mixer.ping()
-  #mixer.set_value("/ch/01/config/name", "TEST")
-  #mixer.set_value("/ch/01/mix/fader", 0.5)
+  # initialize connection to Behringer mixer
+  #mixer = x32.BehringerX32("127.0.0.1", 10330, False)
+  #mixer.ping()
 
   # parse MIDI inevents
   try:
@@ -50,16 +48,43 @@ def main():
             MIDI_databyte1  = event.midi_bytes[0]
             MIDI_databyte2  = event.midi_bytes[1]
 
-        if MIDI_statusbyte == 0XB0:
-            channel = MIDI_databyte1
-            mixer.set_value(f'/ch/{channel:#02}/mix/fader', [MIDI_databyte2 / 127], False)
-            print(f'/ch/{channel:#02}/mix/fader')
+        # TEST
+        t = nanoKONTROL_MIDI_lookup()
+        c = (MIDI_statusbyte, MIDI_databyte1)
+        if c in t:
+            print(t[c])
 
-        event_s = " ".join(f"{b:02X}" for b in event.midi_bytes)
-        print(f"{event.source!s:7} {event_s}")
+            if MIDI_statusbyte == 0XB0:
+                channel = MIDI_databyte1
+                #mixer.set_value(f'/ch/{channel:#02}/mix/fader', [MIDI_databyte2 / 127], False)
+                #print(f'/ch/{channel:#02}/mix/fader')
+
+        #event_s = " ".join(f"{b}" for b in event.midi_bytes)
+        #print(f"{event_s}")
   except KeyboardInterrupt:
     pass
 
+def nanoKONTROL_MIDI_lookup():
+    # (scene, type, value), types: "f" is fader, "d" is dial
+    return {(0XB0, 2): (0, "f", 0), (0XB0, 3): (0, "f", 1), (0XB0,  4): (0, "f", 2), (0XB0,  5): (0, "f", 3), (0XB0, 6): (0, "f", 4),
+            (0XB0, 8): (0, "f", 5), (0XB0, 9): (0, "f", 6), (0XB0, 12): (0, "f", 7), (0XB0, 13): (0, "f", 8),
+            (0XB0, 14): (0, "d", 0), (0XB0, 15): (0, "d", 1), (0XB0, 16): (0, "d", 2), (0XB0, 17): (0, "d", 3), (0XB0, 18): (0, "d", 4),
+            (0XB0, 19): (0, "d", 5), (0XB0, 20): (0, "d", 6), (0XB0, 21): (0, "d", 7), (0XB0, 22): (0, "d", 8),
+
+            (0XB0, 42): (1, "f", 0), (0XB0, 43): (1, "f", 1), (0XB0, 50): (1, "f", 2), (0XB0, 51): (1, "f", 3), (0XB0, 52): (1, "f", 4),
+            (0XB0, 53): (1, "f", 5), (0XB0, 54): (1, "f", 6), (0XB0, 55): (1, "f", 7), (0XB0, 56): (1, "f", 8),
+            (0XB0, 57): (1, "d", 0), (0XB0, 58): (1, "d", 1), (0XB0, 59): (1, "d", 2), (0XB0, 60): (1, "d", 3), (0XB0, 61): (1, "d", 4),
+            (0XB0, 62): (1, "d", 5), (0XB0, 63): (1, "d", 6), (0XB0, 65): (1, "d", 7), (0XB0, 66): (1, "d", 8),
+
+            (0XB0,  85): (2, "f", 0), (0XB0,  86): (2, "f", 1), (0XB0,  87): (2, "f", 2), (0XB0,  88): (2, "f", 3), (0XB0,  89): (2, "f", 4),
+            (0XB0,  90): (2, "f", 5), (0XB0,  91): (2, "f", 6), (0XB0,  92): (2, "f", 7), (0XB0,  93): (2, "f", 8),
+            (0XB0,  94): (2, "d", 0), (0XB0,  95): (2, "d", 1), (0XB0,  96): (2, "d", 2), (0XB0,  97): (2, "d", 3), (0XB0, 102): (2, "d", 4),
+            (0XB0, 103): (2, "d", 5), (0XB0, 104): (2, "d", 6), (0XB0, 105): (2, "d", 7), (0XB0, 106): (2, "d", 8),
+
+            (0XB0,  7): (3, "f", 0), (0XB1,  7): (3, "f", 1), (0XB2,  7): (3, "f", 2), (0XB3,  7): (3, "f", 3), (0XB4,  7): (3, "f", 4),
+            (0XB5,  7): (3, "f", 5), (0XB6,  7): (3, "f", 6), (0XB7,  7): (3, "f", 7), (0XB8,  7): (3, "f", 8),
+            (0XB0, 10): (3, "d", 0), (0XB1, 10): (3, "d", 1), (0XB2, 10): (3, "d", 2), (0XB3, 10): (3, "d", 3), (0XB4, 10): (3, "d", 4),
+            (0XB5, 10): (3, "d", 5), (0XB6, 10): (3, "d", 6), (0XB7, 10): (3, "d", 7), (0XB8, 10): (3, "d", 8)}
 
 if __name__ == '__main__':
     main()
