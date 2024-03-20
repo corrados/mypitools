240a2462148b34ea0d66c09eab688125af2fd0ba
Volker Fischer <corrados@users.noreply.github.com>
2024-03-20 17:23:00 +0100

    added some untested code to record XR18 script

--- projects/record_xr18.sh
+++ projects/record_xr18.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-AUDIODEV="hw:XR18,0"; rec -c 18 recording.wav
+while [ 1 ]
+do
+
+  # check for XR18 connected
+  if [ $(aplay -l|grep "X18XR18") -eq 1 ]
+
+    # XR18 connected, we are no longer in edrumulus mode -> kill all edrumulus processes
+    killall ecasound
+    killall drumgizmo
+    killall jackd
+
+    # record multi-channel audio from XR18
+    AUDIODEV="hw:XR18,0"; rec -c 18 recording.wav
+
+  fi
+
+  sleep 10
+
+done
 
