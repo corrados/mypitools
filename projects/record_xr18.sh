c3643b1aa373f6d2ff23352710a3c793cbd95f7c
Volker Fischer <corrados@users.noreply.github.com>
2024-03-21 08:53:38 +0100

    initially, try out if edrumulus tasks must not be killed

--- projects/record_xr18.sh
+++ projects/record_xr18.sh
@@ -6,10 +6,10 @@ do
   # check for XR18 connected
   if [ $(aplay -l|grep -c "X18XR18") -eq 1 ]; then
 
-    # XR18 connected, we are no longer in edrumulus mode -> kill all edrumulus processes
-    killall ecasound
-    killall drumgizmo
-    killall jackd
+    ## XR18 connected, we are no longer in edrumulus mode -> kill all edrumulus processes
+    #killall ecasound
+    #killall drumgizmo
+    #killall jackd
 
     FILENAME=/home/pi/$(date +"%Y%m%d_%H%M").wav
     echo recording to $FILENAME
