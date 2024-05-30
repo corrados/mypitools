5475189f4c8ed1be8fb6c07e4cacdb537f95333f
Volker Fischer <corrados@users.noreply.github.com>
2024-05-30 10:06:11 +0200

    update

--- projects/bodymetrics.py
+++ projects/bodymetrics.py
@@ -21,24 +21,27 @@ import sys, sqlite3, datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 
-# settings
+# settings/initializations
 target_scale = 79
-database_band = sys.argv[1] + "/Gadgetbridge"
-database_scale = sys.argv[1] + "/openScale.db"
+min_scale = 72
+path = sys.argv[1]
+database_bands = [path + "/Gadgetbridge"]#, path + "/Gadgetbridge_20240308-160840"]
+database_scale = path + "/openScale.db"
+data = []
 
 # Band Data --------------------------------------------------------------------
-con = sqlite3.connect(database_band)
-cursor = con.cursor()
-data = []
-cursor.execute("SELECT * FROM MI_BAND_ACTIVITY_SAMPLE")
-rows = cursor.fetchall()
-for row in rows:
-  rate = row[6]
-  if rate < 250 and rate > 0:
-    timestamp = row[0]
-    raw_intensity = row[3] / 255 * 40 # convert range to 0 to 40
-    output_date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M')
-    data.append((output_date, rate, raw_intensity, None))
+for database_band in database_bands:
+ con = sqlite3.connect(database_band)
+ cursor = con.cursor()
+ cursor.execute("SELECT * FROM MI_BAND_ACTIVITY_SAMPLE")
+ rows = cursor.fetchall()
+ for row in rows:
+   rate = row[6]
+   if rate < 250 and rate > 0:
+     timestamp = row[0]
+     raw_intensity = row[3] / 255 * 40 # convert range to 0 to 40
+     output_date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M')
+     data.append((output_date, rate, raw_intensity, None))
 
 # Scale Measurements -----------------------------------------------------------
 con = sqlite3.connect(database_scale)
@@ -48,9 +51,10 @@ cursor.execute("SELECT * FROM scaleMeasurements")
 rows = cursor.fetchall()
 for row in rows:
   weight = row[4]
-  timestamp = row[3] / 1000
-  output_date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M')
-  data.append((output_date, None, None, weight))
+  if weight > min_scale:
+    timestamp = row[3] / 1000
+    output_date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M')
+    data.append((output_date, None, None, weight))
 
 # Plot -------------------------------------------------------------------------
 x, a, b, c = zip(*data)
