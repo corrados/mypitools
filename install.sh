1b891c6620536828be5a525efb009463d60de4ae
Volker Fischer <corrados@users.noreply.github.com>
2025-02-02 21:31:28 +0100

    bug fix

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ sudo apt-get autoclean -y
 # SSH ##########################################################################
 if [ -d "/home/pi/.ssh" ]; then
   if [ -f "/home/pi/.ssh/authorized_keys" ]; then
-    if grep -Fxq "PasswordAuthentication yes" /etc/ssh/sshd_config
+    if grep -Fq "PasswordAuthentication yes" /etc/ssh/sshd_config
     then
       read -p "Do you want to disable SSH password login? " -n 1 -r
       echo
