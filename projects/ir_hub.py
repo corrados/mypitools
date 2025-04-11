d970765cfc57cdf66e77f1ba2599ec25ac4edbc4
Volker Fischer <corrados@users.noreply.github.com>
2025-04-11 20:26:14 +0200

    initial version of new file

---
+++ projects/ir_hub.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import struct
+f = open( "/dev/input/event2", "rb" ); # Open the file in the read-binary mode
+while 1:
+  data = f.read(24)
+  print(struct.unpack('4IHHI',data))
+  ###### PRINT FORMAL = ( Time Stamp_INT , 0 , Time Stamp_DEC , 0 ,
+  ######   type , code ( key pressed ) , value (press/release) )
+
