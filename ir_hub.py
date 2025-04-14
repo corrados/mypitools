430eedabb28a7c0515f057354d7cb9cc4c181a6b
Volker Fischer <corrados@users.noreply.github.com>
2025-04-14 21:31:41 +0200

    use color keys for volume and channel

--- ir_hub.py
+++ ir_hub.py
@@ -11,11 +11,11 @@ device_path = None
 state       = "IDLE"
 prev_state  = "IDLE"
 mapping     = None
-alt_func    = False
+alt_func    = True
 press_lock  = threading.Lock()
 ir_lock     = threading.Lock()
 
-state_map = {"RED":"PROJECTOR", "GREEN":"TV", "YELLOW":"LIGHT", "BLUE":"DVD", "POWER":"IDLE"}
+state_map = {"1":"PROJECTOR", "2":"TV", "3":"LIGHT", "4":"DVD", "POWER":"IDLE"}
 
 # scancode to readable button name for Elgato EyeTV remote
 scancode_offset = 4539649
@@ -25,16 +25,16 @@ scancode_map = {0:"POWER", 1:"MUTE", 2:"1", 3:"2", 4:"3", 5:"4", 6:"5", 7:"6", 8
 26:"REWIND", 27:"L", 28:"FORWARD", 29:"STOP", 30:"TEXT", 63:"REC", 64:"HOLD", 65:"SELECT"}
 
 # key mapping from EyeTV remote to other device remote
-map_TV = {"CH+":"TV UP", "CH-":"TV DOWN", "VOL-":"BAR VOLUMEDOWN", "VOL+":"BAR VOLUMEUP", "OK":"TV OK",
+map_TV = {"CH+":"TV UP", "CH-":"TV DOWN", "VOL-":"TV LEFT", "VOL+":"TV RIGHT", "OK":"TV OK",
 "1":"TV 1", "2":"TV 2", "3":"TV 3", "4":"TV 4", "5":"TV 5", "6":"TV 6", "7":"TV 7", "8":"TV 8",
-"9":"TV 9", "0":"TV 0",
-"BACK_LEFT":"TV MENU"}
-map_DVD = {"CH+":"DVD UP", "CH-":"DVD DOWN", "VOL-":"BAR VOLUMEDOWN", "VOL+":"BAR VOLUMEUP", "OK":"DVD OK",
+"9":"TV 9", "0":"TV 0", "BACK_LEFT":"TV MENU", "RED":"BAR VOLUMEUP", "YELLOW":"BAR VOLUMEDOWN",
+"GREEN":"TV CHANNELUP", "BLUE":"TV CHANNELDOWN"}
+map_DVD = {"CH+":"DVD UP", "CH-":"DVD DOWN", "VOL-":"DVD LEFT", "VOL+":"DVD RIGHT", "OK":"DVD OK",
 "1":"DVD 1", "2":"DVD 2", "3":"DVD 3", "4":"DVD 4", "5":"DVD 5", "6":"DVD 6", "7":"DVD 7", "8":"DVD 8",
-"9":"DVD 9", "0":"DVD 0",
-"BACK_LEFT":"DVD MENU"}
-map_PROJECTOR = {"VOL-":"BAR VOLUMEDOWN", "VOL+":"BAR VOLUMEUP", "MUTE":"BAR MUTE"}
+"9":"DVD 9", "0":"DVD 0", "BACK_LEFT":"DVD MENU", "RED":"BAR VOLUMEUP", "YELLOW":"BAR VOLUMEDOWN"}
+map_PROJECTOR = {"RED":"BAR VOLUMEUP", "YELLOW":"BAR VOLUMEDOWN", "MUTE":"BAR MUTE"}
 map_LIGHT = {"CH+":"LED BRIGHTER", "CH-":"LED DIMMER", "VOL-":"LED DIMMER", "VOL+":"LED BRIGHTER",
+"RED":"LED BRIGHTER", "YELLOW":"LED DIMMER", "GREEN":"LED BRIGHTER", "BLUE":"LED DIMMER",
 "1":"LED BLUE", "2":"LED GREEN", "3":"LED RED", "4":"LED ORANGE", "5":"LED LIGHTERORANGE",
 "6":"LED CORAL", "7":"LED YELLOW", "8":"LED LIGHTERGREEN", "9":"LED TURQUOISE", "0":"LED AQUA",
 "BACK_LEFT":"LED NAVY", "OK":"LED WHITE"}
@@ -49,7 +49,7 @@ def watch_input():
         if event_type == 4 and code == 4: # MSC_SCAN
           scancode = value - scancode_offset
           button_name = scancode_map.get(scancode, f"UNKNOWN ({scancode})")
-          if scancode != last_scancode or time.time() - last_time > 0.2 or button_name in {"VOL+", "VOL-"}:
+          if scancode != last_scancode or time.time() - last_time > 0.2 or button_name in {"RED", "YELLOW"}:
             last_scancode = scancode
             threading.Thread(target=on_button_press, args=(button_name,)).start()
           last_time = time.time()
@@ -62,13 +62,14 @@ def on_button_press(button_name):
     if button_name == "SELECT":
       alt_func = True
       return
-    if button_name in state_map:
+    if (alt_func or button_name == "POWER") and button_name in state_map:
       if state == state_map[button_name]:
         print("Help action requested -> do transition again")
         state = prev_state;
       match button_name:
         case "POWER":
-          mapping = None
+          mapping  = None
+          alt_func = True # per definition, to be able to select mode right away
           if state in ("TV", "LIGHT"):
             ir_send_in_thread("LED POWER OFF")
           if state in ("PROJECTOR", "DVD", "TV"):
@@ -79,8 +80,9 @@ def on_button_press(button_name):
               ir_send_in_thread("BEAM POWER OFF")
             if state in ("DVD"):
               ir_send_in_thread("DVD POWER OFF")
-        case "RED": # PROJECTOR -----
-          mapping = map_PROJECTOR
+        case "1": # PROJECTOR -----
+          mapping  = map_PROJECTOR
+          alt_func = False
           if state in ("TV", "LIGHT"):
             ir_send_in_thread("LED POWER OFF")
           if state in ("TV", "DVD"):
@@ -92,8 +94,9 @@ def on_button_press(button_name):
             ir_send_in_thread("BAR BLUETOOTH") # powers it on, too
           if not state in ("DVD"):
             threading.Thread(target=switch_projector_on_with_input_select, args=("HDMI2",)).start()
-        case "GREEN": # TV -----
-          mapping = map_TV
+        case "2": # TV -----
+          mapping  = map_TV
+          alt_func = False
           if not state in ("LIGHT"):
             ir_send_in_thread("LED POWER ON")
           if state in ("PROJECTOR", "DVD"):
@@ -103,8 +106,9 @@ def on_button_press(button_name):
           else:
             ir_send_in_thread("BAR OPTICAL") # powers it on, too
           threading.Thread(target=switch_tv_on).start() # special function needed for TV ON
-        case "YELLOW": # LIGHT -----
-          mapping = map_LIGHT
+        case "3": # LIGHT -----
+          mapping  = map_LIGHT
+          alt_func = False
           if state in ("PROJECTOR", "DVD", "TV"):
             ir_send_in_thread("BAR POWER OFF")
             if state in ("TV"):
@@ -115,8 +119,9 @@ def on_button_press(button_name):
               ir_send_in_thread("DVD POWER OFF")
           if not state in ("TV"):
             ir_send_in_thread("LED POWER ON")
-        case "BLUE": # DVD -----
-          mapping = map_DVD
+        case "4": # DVD -----
+          mapping  = map_DVD
+          alt_func = False
           if state in ("TV", "LIGHT"):
             ir_send_in_thread("LED POWER OFF")
             if state in ("TV"):
@@ -131,7 +136,7 @@ def on_button_press(button_name):
     else:
       if mapping:
         ir_send_in_thread(f"{mapping.get(button_name, 'UNKNOWN')}")
-    alt_func = False # reset alternate function flag at the end of the function
+        alt_func = False # reset alternate function flag at the end of the function after send command
 
 def switch_tv_on():
   ir_send_in_thread("TV POWER ON") # immediate attempt
