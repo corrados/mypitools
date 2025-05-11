924d318452dc47e4500e08718086d92b52e011e5
Volker Fischer <corrados@users.noreply.github.com>
2025-05-11 12:22:29 +0200

    fixes with TV interference

--- ir_hub.py
+++ ir_hub.py
@@ -163,25 +163,26 @@ def on_button_press(button_name):
         mapping   = None
         alt_func  = True # special case: per definition True, to be able to select mode right away
         led_is_on = False
+        ir_send_in_thread("TV POWEROFF") # need to be first (interference with IR signal)
+        time.sleep(0.2)                  # need to be first (interference with IR signal)
         if state in ("PROJECTOR", "DVD", "TV", "TVFIRE"):
           ir_send_in_thread("BAR POWER") # doesn't matter if incorrect, switches off after 15 minutes idle anyway
         if state in ("PROJECTOR", "DVD"):
           threading.Thread(target=switch_projector_off).start()
-        ir_send_in_thread("TV POWEROFF")
         ir_send_in_thread("DVD POWEROFF")
         ir_send_in_thread("LED POWEROFF")
       elif button_name == "1": # PROJECTOR -----
         mapping   = map_PROJECTOR
         led_is_on = False
+        ir_send_in_thread("TV POWEROFF") # need to be first (interference with IR signal)
+        time.sleep(0.2)
+        threading.Thread(target=switch_bar_on, args=("BLUETOOTH",)).start()
         if not state in ("DVD"):
           threading.Thread(target=switch_projector_on_with_input_select, args=("PROJECTOR", "HDMI1",)).start()
         else:
           pass #ir_send_in_thread("BEAM HDMI1", 1)
-        threading.Thread(target=switch_bar_on, args=("BLUETOOTH",)).start()
         ir_send_in_thread("LED POWEROFF")
-        ir_send_in_thread("TV POWEROFF")
         ir_send_in_thread("DVD POWEROFF")
-        ir_send_in_thread("LED POWEROFF") # sometimes, LED did not turn off -> do power off again
       elif button_name == "2" or button_name == "5": # TV/TVFIRE -----
         mapping   = map_TV
         led_is_on = True
@@ -205,18 +206,21 @@ def on_button_press(button_name):
       elif button_name == "3": # LIGHT -----
         mapping   = map_LIGHT
         led_is_on = True
-        ir_send_in_thread("LED POWERON")
+        ir_send_in_thread("TV POWEROFF") # need to be first (interference with IR signal)
+        time.sleep(0.2)                  # need to be first (interference with IR signal)
         if state in ("PROJECTOR", "DVD", "TV", "TVFIRE"):
           ir_send_in_thread("BAR POWER") # doesn't matter if incorrect, switches off after 15 minutes idle anyway
+        ir_send_in_thread("LED POWERON")
         if state in ("PROJECTOR", "DVD"):
           threading.Thread(target=switch_projector_off).start()
-        ir_send_in_thread("TV POWEROFF")
         ir_send_in_thread("DVD POWEROFF")
         ir_send_in_thread("LED WHITE") # initial state of LED
         threading.Thread(target=led_max_brightness).start()
       elif button_name == "4": # DVD -----
         mapping   = map_DVD
         led_is_on = False
+        ir_send_in_thread("TV POWEROFF") # need to be first (interference with IR signal)
+        time.sleep(0.2)                  # need to be first (interference with IR signal)
         ir_send_in_thread("DVD POWERON")
         threading.Thread(target=switch_bar_on, args=("BLUETOOTH",)).start()
         if not state in ("PROJECTOR"):
@@ -224,8 +228,6 @@ def on_button_press(button_name):
         else:
           pass #ir_send_in_thread("BEAM HDMI2", 1)
         ir_send_in_thread("LED POWEROFF")
-        ir_send_in_thread("TV POWEROFF")
-        ir_send_in_thread("LED POWEROFF") # sometimes, LED did not turn off -> do power off again
       prev_state = state
       state      = state_map[button_name]
     else:
