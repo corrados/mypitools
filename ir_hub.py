422e9a2cdedbb5813b45b24c82d902c6f90bee1f
Volker Fischer <corrados@users.noreply.github.com>
2025-05-23 21:16:04 +0200

    tests for socket time-outs

--- ir_hub.py
+++ ir_hub.py
@@ -84,8 +84,12 @@ def playstation_remote_input():
   sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_RAW, 1)
   sock.bind((0,))
   sock.setsockopt(0, 2, struct.pack("LLLH", (1 << HCI_EVENT_PKT) | (1 << HCI_ACLDATA_PKT), 0xffffffff, 0xffffffff, 0))
+  sock.settimeout(0.1)
   while True:
-    pkt = sock.recv(2048)
+    try:
+      pkt = sock.recv(2048)
+    except socket.timeout:
+      pass
     if pkt[0] == HCI_ACLDATA_PKT and pkt[-7:-2] == bytes.fromhex("ffffffffff"):
       if pkt[-8] == 255 and pkt[-2] == 0:
         pass # TODO act on button up detected
